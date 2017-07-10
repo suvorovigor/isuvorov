9d6585a05535f3b3e35eaab078ad7d591bee2fe1
suvorovigor <Suvor_97@mail.ru>
2017-07-10 16:43:33 -0400

    task 4086

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 /**
- * Class Класс для вычисления арифметических операций + - * / ^.
- * @autor suvorovigor
+ * Class Класс для вычисление арифметических операций + - * / ^.
+ * @author suvorovigor
  * @since 10.07.2017
  * @version 1
  */
@@ -13,7 +13,7 @@ public class Calculate {
 
 /** 
  * Конструктор, вывод строки в консоль.
- * @param arg - arg.
+ * @param args - arg.
  */
 
 	public static void main(String[] args) {
